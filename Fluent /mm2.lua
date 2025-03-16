47ed29c5bf9ef3c7a76bb8a3ca7c9e4562318e74
FRONT HUB <kaidmHub@gmail.com>
2025-03-16 04:43:06 +0300

    Update mm2.lua

--- Fluent /mm2.lua
+++ Fluent /mm2.lua
@@ -1,22 +1,29 @@
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
 local Window = Fluent:CreateWindow({
-    Title = "Fluent " .. Fluent.Version,
-    SubTitle = "by dawid",
+    Title = "Script mm2",
+    SubTitle = "By Front",
     TabWidth = 160,
     Size = UDim2.fromOffset(580, 460),
     Acrylic = true, -- The blur may be detectable, setting this to false disables blur entirely
-    Theme = "Dark", -- Light or Darker or Rose
+    Theme = "Darker", -- Light or Darker or Rose
     MinimizeKey = Enum.KeyCode.LeftControl -- Used when theres no MinimizeKeybind
 })
 
 -- Fluent provides Lucide Icons, they are optional
 local Tabs = {
-    Main = Window:AddTab({ Title = "Main", Icon = "rbxassetid://131460145238951" }),
-    Settings = Window:AddTab({ Title = "Acount Front", Icon = "acount" })
+    Main = Window:AddTab({ Title = "Main", Icon = "rbxassetid://131460145238951" })
 }
 
-local Section = Tab:AddSection("Section Name")
+local Section = Main:AddSection("Section Name")
 Section:AddParagraph({
-    Title = "Mainm🙄"
+    Title = "Fly"
+})
+
+Tab:AddButton({
+    Title = "Button",
+    Description = "Very important button",
+    Callback = function()
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/Front-Evill/Script-Hub/refs/heads/main/Fly.lua.txt"))()
+    end
 })
