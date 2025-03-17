341b6b81743fc4b0a88112f5dd269e1e156c25c4
FRONT HUB <kaidmHub@gmail.com>
2025-03-17 22:03:59 +0300

    Update Icon.lua

--- Fluent /Icon.lua
+++ Fluent /Icon.lua
@@ -1,3 +1,8 @@
 return {
-  ["Homa-For-Main"] = "rbxassetid://103167069627270"
+  ["Icon-Home-For-Main"] = "rbxassetid://103167069627270",
+  ["Icon-Compass-For-Tab-Teleport"] = "rbxassetid://139926030461097",
+  ["Icon-server-For-Tab-servers"] = "rbxassetid://93989683556149",
+  ["Icon-server-Crash-For-Tab-servers"] = "rbxassetid://133042878145428",
+  ["Icon-User-Round-For-Tab-Player"] = "rbxassetid://112831378967496",
+  ["Icon-Seting-For-Tab-Seting"] = "rbxassetid://10709810948"
 }
