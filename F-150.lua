7fefaa5465b548de36be58b584624b39fc6d0abf
FRONT EVILL <kaidmHub@gmail.com>
2025-04-24 22:11:42 +0300

    Update F-150.lua

--- F-150.lua
+++ F-150.lua
@@ -20,8 +20,7 @@ local RaritesColor = {
     Default = Vector3.new(106, 106, 106)
 }
 --Function
-
-local player = game.Players.LocalPlayer
+local function Notify(Title,Dis)
     pcall(function()
         Fluent:Notify({Title = tostring(Title),Content = tostring(Dis),Duration = 5})
         local sound = Instance.new("Sound", game.Workspace) sound.SoundId = "rbxassetid://3398620867" sound.Volume = 1 sound.Ended:Connect(function() sound:Destroy() end) sound:Play()
